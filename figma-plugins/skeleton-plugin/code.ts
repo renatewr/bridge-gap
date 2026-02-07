b3293a33134a3cfe9ff04c7ade06847bbe533a75
Renate Winther Ravnaas <renate.ravnaas@amedia.no>
2026-02-07 13:46:25 +0100

    Just linting files and adds biome for linting

--- figma-plugins/skeleton-plugin/code.ts
+++ figma-plugins/skeleton-plugin/code.ts
@@ -3,21 +3,21 @@ figma.showUI(__html__, { width: 300, height: 200 });
 
 // Handle messages from the UI
 figma.ui.onmessage = (msg: { type: string }) => {
-  if (msg.type === 'create-rectangle') {
-    const rect = figma.createRectangle();
-    rect.x = figma.viewport.center.x;
-    rect.y = figma.viewport.center.y;
-    rect.resize(100, 100);
-    rect.fills = [{ type: 'SOLID', color: { r: 0.5, g: 0.5, b: 1 } }];
+	if (msg.type === "create-rectangle") {
+		const rect = figma.createRectangle();
+		rect.x = figma.viewport.center.x;
+		rect.y = figma.viewport.center.y;
+		rect.resize(100, 100);
+		rect.fills = [{ type: "SOLID", color: { r: 0.5, g: 0.5, b: 1 } }];
 
-    figma.currentPage.appendChild(rect);
-    figma.currentPage.selection = [rect];
-    figma.viewport.scrollAndZoomIntoView([rect]);
+		figma.currentPage.appendChild(rect);
+		figma.currentPage.selection = [rect];
+		figma.viewport.scrollAndZoomIntoView([rect]);
 
-    figma.notify('Rectangle created!');
-  }
+		figma.notify("Rectangle created!");
+	}
 
-  if (msg.type === 'cancel') {
-    figma.closePlugin();
-  }
+	if (msg.type === "cancel") {
+		figma.closePlugin();
+	}
 };
